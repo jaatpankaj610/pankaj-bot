c2d58c8bf03046823285f808cc0a496b44c482c4
jaatpankaj610 <12jaat13@gmail.com>
2026-07-23 18:40:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # 2. कॉन्फ़िगरेशन
-TOKEN = "7908449655:AAFU5S4qmv223fQ0ffK6g80acVxGX3SpO7A"
+TOKEN = ""
 GITHUB_URL = "https://raw.githubusercontent.com/jaatpankaj610/paid-quiz-app/main/quiz_database.json"
 WEBHOOK_URL = f"https://bankerbot-mdzw.onrender.com/{TOKEN}"
 
